96b08bad9e23fec06bb0027086b91572978a09c1
Kaitlyn Mohr <kpmohr@gmail.com>
2017-04-17 09:24:11 -0400

    little things

--- EndMessage.elm
+++ EndMessage.elm
@@ -8,15 +8,15 @@ endMessage : Int -> String
 endMessage percentage =
     if percentage == 100 then
         "You're a Turtle Expert!"
-    else if percentage > 90 then
+    else if percentage >= 90 then
         "A Wise Turtle You Are."
-    else if percentage > 80 then
+    else if percentage >= 80 then
         "You should pay more attention to your turtles!"
-    else if percentage > 70 then
+    else if percentage >= 70 then
         "Are you sure you're a turtle?"
-    else if percentage > 60 then
+    else if percentage >= 60 then
         "Do you even like turtles?"
-    else if percentage > 50 then
+    else if percentage >= 50 then
         "You must be a Shoebill!"
     else
         "Honestly....just try again..."
